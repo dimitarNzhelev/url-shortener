56d053706bebd8015003f78bdd8252f19fe11004
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-10-17 17:53:28 +0300

    updated: updated the code to work with destructed db url

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -8,7 +8,13 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+const conn = globalForDb.conn ?? postgres({
+  host: env.DB_HOST,
+  port: Number(env.DB_PORT),
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
+});
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
